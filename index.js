80471f43b22e4948e38fa9c49d5401540cac6587
SahusBhandary <sahusbhandary04@gmail.com>
2024-02-10 01:41:01 -0500

    Test GPT

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 import OpenAI from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 
 const openai = new OpenAI({
     organization: "org-luARjjyTeb5FLVVoL8Ok8PRy",
-    apiKey: "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",
+    apiKey: "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",
 });
 
-const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [
-        {role: "user", content: "What is 2 + 2?"},
-    ]
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+app.use(cors());
 
-})
+app.post("/", async (req, res) => {
 
-console.log(completion.choices[0].message);
+    const { message } = req.body;
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            {role: "user", content: `${message}`},
+        ]
+    })
+
+    res.json({
+        completion: completion.choices[0].message
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
