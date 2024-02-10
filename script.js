4c54147bc60da2f7dfed35e6c5fd59fe773b7075
Meshane Peiris <meshanepeiris@gmail.com>
2024-02-10 14:30:10 -0500

    lots of changes to landing page, added new pictures, javascript works

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const startButton = document.querySelector('#startButton');
 const codeSection = document.querySelector('#appearingCode');
+const input = document.querySelector('#textBox');
+const submitButton = document.querySelector('#submitButton');
+const chat = document.querySelector('#chat');
 const box = document.querySelector('#box');
-
+const hero = document.querySelector('#hero')
 const array = [
-    "Initializing C:/Unauthorized_User/Secret/PROJECTNAME.exe", 
+    "Initializing C:/SBUHacker_User/TopSecret/PROJECTNAME.exe", 
     "Access Denied.",
     "Attempting to bypass firewall...",
-    "Firewall is impenetrable. Mission failed. ",
-    "Firewall is impenetrable. Mission failed. ",
-    "Firewall is impenetrable. Mission failed. ",
+    "Firewall is impenetrable.",
+    "Mission failed. ",
+    "Attempting to bypass firewall again...",
+    "Firewall is impenetrable.",
     "Mission success. ",
     "Executing program PROJECTNAME.exe",
     "API executed successfully Starting PROJECTNAME.exe"
-
 ];
-
 let intervalId;
 let iterations = 0;
 
@@ -24,9 +26,11 @@ function writeCode() {
         codeSection.innerText += "\n" + array[iterations] ;
         iterations++;
 
-        if (iterations >= 9) {
+        if (iterations >= 10) {
             clearInterval(intervalId);
-            box.innerText = "Add short description of PROJECT as well as a textbox for input tmr";
+            document.getElementById("desc").style.display = "block";
+            document.getElementById("box").style.display = "block";
+            
         }
     
     }, randomDelayMaker());
@@ -36,8 +40,40 @@ function writeCode() {
 }
 
 function randomDelayMaker() {
-    let delay = 800;
+    let delay = 80;
     return delay;
 }
 
+let counter = 0;
+function writeChat() {
+    if (counter == 0 && input.value != "SBUHACKS") {
+
+    }
+    else if (counter == 0 && input.value == "SBUHACKS") {
+        counter = 1;
+        const welcome = document.createElement('p');
+        welcome.style.textAlign = "left";
+        welcome.style.paddingLeft = "40px";
+        welcome.innerText = "\nHi. I am Project Name Chat\n\n";
+        chat.appendChild(welcome);
+    
+    }
+    else {
+        counter = 1;
+        const newText = document.createElement('p');
+        newText.innerText = "\n";
+        newText.innerText += input.value;
+        newText.innerText += "\n\n";
+        chat.appendChild(newText);
+        const newBotText = document.createElement('p');
+        newBotText.innerText = "Response\n\n";
+        newBotText.style.textAlign = "left";
+        newBotText.style.paddingLeft = "40px";
+        chat.append(newBotText);
+
+        
+    }
+}
+
 startButton.addEventListener('click', writeCode);
+submitButton.addEventListener('click', writeChat);
